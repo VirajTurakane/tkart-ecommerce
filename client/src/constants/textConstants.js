dbb5eb08e5715ae0e6976e2dae1006e507837bb6
Viraj Turkane <turakaneviraj@gmail.com>
2025-06-30 16:49:50 +0530

    url changed

--- client/src/constants/textConstants.js
+++ client/src/constants/textConstants.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:3000/api/v1/";
+export const url = "tkart-ecommerce.vercel.app/api/v1/";
 export const addedToWishlist = "Added To Wishlist";
 export const removedFromWishlist = "Removed From Wishlist";
 export const addedToCart = "Product Added To Cart";
