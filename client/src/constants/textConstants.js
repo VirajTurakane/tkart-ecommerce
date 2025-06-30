ead3ec39631c9f918938a047fefd79242f41ffc3
Viraj Turkane <149132856+VirajTurakane@users.noreply.github.com>
2025-06-30 17:52:39 +0530

    Update textConstants.js

--- client/src/constants/textConstants.js
+++ client/src/constants/textConstants.js
@@ -1,4 +1,4 @@
-export const url = "tkart-ecommerce.vercel.app/api/v1/";
+export const url = "https://tkart-ecommerce.onrender.com/api/v1/";
 export const addedToWishlist = "Added To Wishlist";
 export const removedFromWishlist = "Removed From Wishlist";
 export const addedToCart = "Product Added To Cart";
