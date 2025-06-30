ef1d55ae786c1e76c3fcdfd00491ce648cb5709a
Viraj Turkane <149132856+VirajTurakane@users.noreply.github.com>
2025-06-30 18:00:29 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://tkart-ecommerce-app.vercel.app",
+    origin: "https://tkart.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
