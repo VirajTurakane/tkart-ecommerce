0a8d80a7492f1fce14b3ee35d57463e4796a3274
Viraj Turkane <turakaneviraj@gmail.com>
2025-06-30 19:03:45 +0530

    wishlist updated

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -17,7 +17,7 @@ const Wishlist = () => {
   return (
     <div className="flex flex-col w-full h-full gap-4">
       <Header title={"Wishlist"} />
-      {wishlist.length === 0 ? (
+      {wishlist?.length === 0 ? (
         <Empty message={"Wishlist is empty."} />
       ) : (
         <div className="px-[3%] sm:px-[4%] md:px-[5%] flex flex-col gap-4">
