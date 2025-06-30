d5e25e5b29b761639fbf26d7b0f6417456cf7f38
Viraj Turkane <turakaneviraj@gmail.com>
2025-06-30 17:01:05 +0530

    app.js updated

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "https://tkart-ecommerce-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
